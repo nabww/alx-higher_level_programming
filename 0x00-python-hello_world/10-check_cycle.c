0e1f71010b7194ffa228fe245d756e538c232f83
nwambulwa <nwambulwa@yahoo.com>
2023-01-04 03:34:02 -0800

    number 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
