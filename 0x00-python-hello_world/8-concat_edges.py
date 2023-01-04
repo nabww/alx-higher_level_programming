808efa595377f57110cc2918e94348f5b3343bee
nwambulwa <nwambulwa@yahoo.com>
2023-01-04 03:06:31 -0800

    number 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming lanuage that combines remarkable power with very clear syntax"
+required_string = str[str.index("object-oriented programming"):str.index("lanuage")]
+print(required_string + "\n")
