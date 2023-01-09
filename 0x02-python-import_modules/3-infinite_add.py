424fe4d8b2d1ed8e08862bc5f84d2eb7db929ba4
nwambulwa <nwambulwa@yahoo.com>
2023-01-09 04:13:46 -0800

    correct number 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-result = 0
-
-for argument in arguments:
-    result += int(argument)
-
-print(result)
+if __name__ == "__main__":
+    import sys
+    result = sum(int(sys.argv[x]) for x in range(1, len(sys.argv)))
+    print("{}".format(result))
