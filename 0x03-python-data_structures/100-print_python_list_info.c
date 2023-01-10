2d7f9de20d0ce011791f23ccb3c824a9987070b7
nwambulwa <nwambulwa@yahoo.com>
2023-01-10 01:14:42 -0800

    unresolved error on last task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+    for (Py_ssize_t i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+    }
+}
+
