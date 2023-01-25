8b2e75b798ed9063c36810cb1da614ff451dc999
nwambulwa <nwambulwa@yahoo.com>
2023-01-24 21:35:36 -0800

    number 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        i = 0
-        while i < x:
-            print(my_list[i], end='')
-            i += 1
+        count = 0
+        while count < x:
+            print(my_list[count], end='')
+            count = count + 1
         print()
-        return i
+        return count
     except:
         print()
-        return i
+        return count
 
