02af638ecf9ca187e7ed021aba48d7e42f486fe1
nwambulwa <nwambulwa@yahoo.com>
2023-02-07 05:49:50 -0800

    number 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+read_file function module.
+
+define read_file_function.
+
+"""
+
+
+def read_file(filename=""):
+    """Read text file (UTF8) and print to stdout
+    filename (file):
+    """
+
+    with open(filename, encoding="UTF 8") as myfile:
+        print(myfile.read(), end="")
