d089a204944c517ea8f2a57c65c091e61436a992
nwambulwa <nwambulwa@yahoo.com>
2023-04-13 23:51:35 -0700

    correct 102

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -17,3 +17,4 @@ if __name__ == "__main__":
     session = Session()
 
     for city in session.query(City).order_by(City.id):
+        print("{}:{} -> {}".format(city.id, city.name, city.state.name)
